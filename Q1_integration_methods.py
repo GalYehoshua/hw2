b78461f93de34b9bd8dd7c086abf8e97473cd422
Gal <gal.yehoshua@gmail.com>
2024-02-01 16:42:57 +0200

    Romberging

--- Q1_integration_methods.py
+++ Q1_integration_methods.py
@@ -12,15 +12,16 @@ def print_function():
     plt.show()
 
 
+def width(iteration):
+    return 1.0 / 2 ** iteration
+
+
 def trapeziod_integrals(err=10 ** -6, func=f):
     integrals = [0, 1 * 0.5 * (func(1) + func(0))]
     i = 1  # number of subdivision will be 2 ** i
 
     # calc the width of the i'th iteration, i=0 is full interval
-    def width(iteration):
-        return 1.0 / 2 ** iteration
-
-    assert width(1) == 1/2
+    assert width(1) == 1 / 2
 
     while True:
         new_integral = 0.5 * integrals[-1] + width(i) * sum([func(k * width(i)) for k in range(1, 2 ** i, 2)])
@@ -34,6 +35,38 @@ def trapeziod_integrals(err=10 ** -6, func=f):
             return integrals[-1]
 
         i += 1
+
+
+def romberg(err=1e-6, func=f, start=0, end=1):
+    trapeziod_integrals = {1: 0.5 * (func(start) + func(end))}
+    trapeziod_integrals[2] = 0.5 * trapeziod_integrals[1] + 0.5 * (func(0) + func(0.5) + func(1))
+    romberg_integrals = {(1, 1): trapeziod_integrals[1],
+                         (2, 1): trapeziod_integrals[2]}
+
+    romberg_integrals[(2, 2)] = romberg_integrals[(2, 1)] + 1 / 3 * (
+                romberg_integrals[(2, 1)] - romberg_integrals[(1, 1)])
+    i = 3
+    while True:
+        # computing trapeziod integrals using eq 2
+        trapeziod_integrals[i] = 0.5 * trapeziod_integrals[i - 1] \
+                                 + width(i) * sum([func(k * width(i)) for k in range(1, 2 ** i, 2)])
+        # setting R_i1 to be I_i
+        romberg_integrals[(i, 1)] = trapeziod_integrals[i]
+        # computing refined romberg integrals using eq 3
+        for m in range(1, i):
+            romberg_integrals[(i, m + 1)] = romberg_integrals[(i, m)] \
+                                            + (romberg_integrals[(i, m) - romberg_integrals[(i - 1, m)]]) / (4 ** m - 1)
+
+        # estimate the err by eq 4.
+        estimate_err = (romberg_integrals[(i, i) - romberg_integrals[(i - 1, i)]]) / (4 ** i - 1)
+        if estimate_err < err:
+            return i, romberg_integrals[(i, i)]
+        i += 1
+
+        # computation did not converge after 1000 iterations, halt! you may cry now :(
+        if i == 1000:
+            print('finish at step 1000 with approx. err', estimate_err)
+            return romberg_integrals
 
 
 print(trapeziod_integrals())
