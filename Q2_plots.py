2fc0ee5fc66f47892d1eb5244a3f3ef4128367fd
Gal <gal.yehoshua@gmail.com>
2024-02-04 21:33:10 +0200

    hmmmm

--- Q2_plots.py
+++ Q2_plots.py
@@ -4,7 +4,7 @@ import numpy as np
 from commons import *
 from Q2_spline import CubicSplinesInter
 
-scales = [0.1, 0.5, 1]
+scales = [0.001, 0.1, 0.5, 1]
 
 
 def a_plots():
@@ -16,6 +16,7 @@ def a_plots():
 
 
 def b_plots():
+    # as num_der is a linear function we can do it directly on the noise.
     x_range = np.linspace(0.5, 10, 99)
     for i, scaling_factor in enumerate(scales):
         plt.subplot(int(f'41{i + 1}'))
@@ -25,39 +26,28 @@ def b_plots():
 
 def c_plots():
     start, end = 0.5, 10
-    x_range = np.linspace(start, end, 100)
-    for i, scale in enumerate(scales[:1]):
-        plt.subplot(int(f'31{i + 1}'))
-        f_noise = f(x_range) + scale * noise
-        curr_spline = CubicSplinesInter(x_range, f_noise)
-        funcs = curr_spline.analytical_spline_der()
-        x, y = compute_list_of_functions(funcs, np.linspace(start, end, len(funcs) + 1), 11)
-        plt.plot(x, y)
+    inter_points = 99 + 1
+    x_range = np.linspace(start, end, inter_points)
+    dx = x_range[1] - x_range[0]
+    smth_ass_func = f
+    for i, scale in enumerate(scales):
+        # plt.subplot(int(f'31{i + 1}'))
+        f_noise = smth_ass_func(x_range) + scale * noise[:inter_points]
+        f_noise_inter_spline = CubicSplinesInter(x_range, f_noise)
+        funcs = f_noise_inter_spline.analytical_spline_der()
+        x, y = compute_list_of_functions(funcs, np.linspace(start, end, len(funcs) + 1), 20)
+        # Removing the original function:
+        y -= smth_ass_func(x)
+
+        plt.plot(x, y, color='orange', label="inter data")
+        plt.plot(np.linspace(0.5, 10, 100)[:99], scale * der(noise, dx), color='g', label="noise der")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title("Noise derivation comparison")
         plt.show()
-
-
-def c2_plots():
-    start, end = 0.5, 10
-    x_range = np.linspace(start, end, 100)
-    noise_spline_inter = CubicSplinesInter(x_range, noise)
-    noise_spline = noise_spline_inter.splines
-
-    # noise inter
-    plt.subplot(211)
-    x, y = compute_list_of_functions(noise_spline, np.linspace(start, end, len(noise_spline) + 1), 4)
-    plt.plot(x, y/y[0])
-
-    print(y[0:33])
-    print(noise[0:33])
-
-    plt.subplot(212)
-    der_noise = noise_spline_inter.analytical_spline_der()
-    x, y = compute_list_of_functions(der_noise, np.linspace(start, end, len(der_noise) + 1), 4)
-    plt.plot(x, y)
-    plt.show()
 
 
 if __name__ == '__main__':
     # a_plots()
     # b_plots()
-    c2_plots()
+    c_plots()
